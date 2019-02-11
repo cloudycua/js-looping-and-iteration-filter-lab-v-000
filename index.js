969537ea04ddd4bf7ecd37fbc1d0fdb5538cbd67
Christine Cua <cloudycua@gmail.com>
2019-02-11 15:46:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function findMatching(list, name) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
+
+function fuzzyMatch(list, letter) {
+  return list.filter(function (driverName) {
+    return driverName.charAt(0).toLowerCase() === letter.toLowerCase();
+  });
+}
